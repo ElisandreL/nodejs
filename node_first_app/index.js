357be5f62f640ada116743b94f4cefb70e7a1bd0
Elisandre <elisandre.laenens@gmail.com>
2021-09-24 16:48:47 +0200

    app_1 finito

--- node_first_app/index.js
+++ node_first_app/index.js
@@ -1,12 +1,18 @@
 // import the NPM dependancy package
+const bodyParser = require("body-parser");
 const express = require("express");
+
 // initialize express() inside and write to the app variable
 const app     = express();
+//Use body-parser
+app.use(bodyParser.json());
 //mport route module and pass your app
-require("./routes/userRoutes"),(app);
+require("./routes/userRoutes")(app);
 //choose what port on which to run server 
 const PORT = 5000;
 //use the app varianme and listen to the port
 app.listen(PORT, () => {
     console.log(`Server running`);
 });
+
+
--- node_first_app/routes/userRoutes.js
+++ node_first_app/routes/userRoutes.js
@@ -1,10 +1,103 @@
 const jsonfile = require('jsonfile');
-const express = require('express');
-const app = express();
+const file_path = "./DB/users.json";
 
-app.get("/users", (req, res) => {
+module.exports = app => {
+
+/*
+    //here is a basic get route which reads data from a local directory json file
+    app.get("/users", (req, res) => {
     console.log("fetching all users");
-    jsonfile.readFile("./DB/users.json", function(err, content) {
+    jsonfile.readFile(file_path, function(err, content) {
         res.send(content);
     });
-});
+});*/
+
+
+    /*
+    //here is a post route which writes data from a local directory json file
+
+  app.post('/users/new', (req, res) => {
+    // let email = req.body.email;
+    // let username = req.body.username;
+    let {email, username} = req.body;
+
+    jsonfile.readFile('./DB/users.json', function(err, content) {
+        content.push({"email": email, "username": username});
+
+        console.log(`added ${email} to DB`);
+
+        jsonfile.writeFile(file_path, content, function(err) {
+            console.log(err);
+        });
+        res.sendStatus(200);
+    });
+})*/
+
+/*
+
+// Here is a delete route for a datastorage
+
+app.delete("/users/destroy", (req, res) => {
+    let email = req.body.email;
+
+    jsonfile.readFile(file_path, function(err, content) {
+        for (var i = content.length - 1; i >= 0; i--) {
+             if (content[i].email === email) {
+                 console.log(`removing ${content[i].email} from DB`);
+                 content.splice(i,1);
+             }
+        }
+
+        jsonfile.writeFile(file_path, content, function(err) {
+            console.log(err);
+        });
+
+        res.sendStatus(200);
+    });
+}); */
+
+/*
+// Know, the turn of a PUT route
+
+app.put("/users", (req, res) => {
+    let user;
+    let username = req.body.username;
+    let email = req.query.email;
+
+    jsonfile.readFile(file_path, function(err, content) {
+        for (var i = content.length - 1; i >= 0; i--) {
+            if (content[i].email === req.query.email) {
+                console.log(`updated user ${req.query.email} has now username : ${username}`);
+                user = content[i];
+                user.username = username;
+            }
+        }
+
+        jsonfile.writeFile(file_path, content, function(err) {
+            console.log(err);
+        });
+    });
+    res.send(user);
+})*/
+
+// Last GET route
+
+app.get('/users', (req, res) => {
+    let user;
+    let email = req.query.email;
+
+    jsonfile.readFile(file_path, function(err, content) {
+        for (var i = content.length - 1; i >= 0; i--) {
+            if (content[i].email === email) {
+                console.log(`found user ${content[i]}`);
+                console.log(content[i]);
+                user = content[i];
+            }
+            
+        }
+
+        res.send(user);
+    })
+})
+
+}
